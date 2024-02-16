c29683c3c74e6ed39fad904d8c3f3bce2476e527
OsakaThx <hoshuacastillo48@gmail.com>
2024-02-16 09:28:48 -0600

    first commit

---
+++ ExamenProblema2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ExamenProblema2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double examen1 = 0;
+            double examen2 = 0;
+            Console.WriteLine("Ingrese la nota del examen 1:");
+            if (double.TryParse(Console.ReadLine(), out examen1))
+            {
+                Console.WriteLine($"La nota del examen 1 es: {examen1}");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un valor numérico válido para la nota del examen 1.");
+            }
+
+            Console.WriteLine("Ingrese la nota del examen 2:");
+            if (double.TryParse(Console.ReadLine(), out examen2))
+            {
+                Console.WriteLine($"La nota del examen 2 es: {examen2}");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un valor numérico válido para la nota del examen 2.");
+                return; 
+            }
+
+
+            double promedio = (examen1 + examen2) / 2;
+            Console.WriteLine($"El promedio de las notas es: {promedio}");
+
+        }
+    }
+}
